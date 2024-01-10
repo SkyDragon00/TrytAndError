95ff4c8de4bf63e6c23a6b2af0a71780435eb8a2
Domenica Escobar <domenica.escobar.moyano@udla.edu.ec>
2024-01-10 15:40:59 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TrytAndError;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TrytAndError;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
